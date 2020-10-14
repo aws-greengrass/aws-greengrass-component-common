87f2663b0f3d2f149af9169aa16a6c91da978881
Gregory Katkov <katkov@amazon.com>
2020-10-13 17:19:02 -0700

    Make serialization of template version safer
    SIM https://t.corp.amazon.com/P40742286
    cr https://code.amazon.com/reviews/CR-36265152

--- src/main/java/com/amazon/aws/iot/greengrass/component/common/RecipeFormatVersion.java
+++ src/main/java/com/amazon/aws/iot/greengrass/component/common/RecipeFormatVersion.java
@@ -4,5 +4,24 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum RecipeFormatVersion {
     @JsonProperty("2020-01-25")
-    JAN_25_2020
+    JAN_25_2020;
+
+    public static RecipeFormatVersion latestVersion() {
+        return JAN_25_2020;
+    }
+
+    /**
+     * Try parse value as enum identifier, if that fails fallback to #defaultVersion
+     *
+     * @param value
+     * @param defaultVersion
+     * @return RecipeFormatVersion corresponding to value or defaultVersion
+     */
+    public static RecipeFormatVersion parseValueOrDefault(final String value, final RecipeFormatVersion defaultVersion) {
+        try {
+            return valueOf(value);
+        } catch (IllegalArgumentException ex) {
+            return defaultVersion;
+        }
+    }
 }
