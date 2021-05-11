892635487e8ba62094e3023ab00a18ae9719f813
Michael Dombrowski <mdombro@amazon.com>
2021-05-11 12:04:15 -0700

    ci: add CI

--- src/test/java/com/amazon/aws/iot/greengrass/component/common/ComponentRecipeDeserializationTest.java
+++ src/test/java/com/amazon/aws/iot/greengrass/component/common/ComponentRecipeDeserializationTest.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
 import org.hamcrest.Matchers;
 import org.hamcrest.collection.IsMapContaining;
 import org.hamcrest.core.Is;
@@ -22,8 +23,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
-
-import lombok.Data;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.aMapWithSize;
